4a89d10ad06d06437f20749cba698a2e036d8ced
Esteban Ordano <esteban@decentraland.org>
2019-06-06 18:01:02 -0300

    split cache code

--- packages/decentraland-loader/lifecycle/lib/scope.ts
+++ packages/decentraland-loader/lifecycle/lib/scope.ts
@@ -13,19 +13,23 @@ const cachedDeltas: Vector2Component[] = []
 export function parcelsInScope(config: ParcelConfigurationOptions, position: Vector2Component): string[] {
   const result: string[] = []
   if (!cachedDeltas.length) {
-    const radius = config.lineOfSightRadius
-    const squaredRadius = radius * radius
-    for (let x = -radius; x <= radius; x++) {
-      for (let y = -radius; y <= radius; y++) {
-        if (x * x + y * y <= squaredRadius) {
-          cachedDeltas.push({ x, y })
-        }
-      }
-    }
-    cachedDeltas.sort((a, b) => squareAndSum(a.x, a.y) - squareAndSum(b.x, b.y))
+    calculateCachedDeltas(config)
   }
   for (let delta of cachedDeltas) {
     result.push(`${position.x + delta.x},${position.y + delta.y}`)
   }
   return result
+}
+
+function calculateCachedDeltas(config: ParcelConfigurationOptions) {
+  const limit = config.lineOfSightRadius
+  const squaredRadius = limit * limit
+  for (let x = -limit; x <= limit; x++) {
+    for (let y = -limit; y <= limit; y++) {
+      if (x * x + y * y <= squaredRadius) {
+        cachedDeltas.push({ x, y })
+      }
+    }
+  }
+  cachedDeltas.sort((a, b) => squareAndSum(a.x, a.y) - squareAndSum(b.x, b.y))
 }
