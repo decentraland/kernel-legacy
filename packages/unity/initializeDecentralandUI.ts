3c3c88e33fed767eed614efaeababed555aae78d
Esteban Ordano <esteban@decentraland.org>
2019-06-15 09:17:58 -0300

    fix: no-case-sensitive

--- packages/unity/initializeDecentralandUI.ts
+++ packages/unity/initializeDecentralandUI.ts
@@ -2,7 +2,7 @@ import { hudWorkerUrl, SceneWorker } from '../shared/world/SceneWorker'
 import { ensureUiApis } from '../shared/world/uiSceneInitializer'
 import { loadedParcelSceneWorkers } from '../shared/world/parcelSceneManager'
 import EngineInterface from './EngineInterface'
-import { UnityScene } from './unityParcelScene'
+import { UnityScene } from './UnityParcelScene'
 
 export default async function initializeDecentralandUI(unityInterface: EngineInterface) {
   const id = 'dcl-ui-scene'
