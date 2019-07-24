98f1fcbaec2b5bd35428ce32a0964b0042df75a0
Pravus <pravus@decentraland.org>
2019-07-24 15:55:00 -0300

    Removed duplicated stun server config

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -107,9 +107,6 @@ export namespace commConfigurations {
       urls: 'stun:stun.l.google.com:19302'
     },
     {
-      urls: 'stun:stun.l.google.com:19302'
-    },
-    {
       urls: 'stun:stun2.l.google.com:19302'
     },
     {
