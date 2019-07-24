29bea82d51c7c62b32436ce64a075525c5bc3f14
Pravus <pravus@decentraland.org>
2019-07-24 16:58:19 -0300

    Updated NFTShape test parcel

--- public/test-parcels/-103.102.nft/game.ts
+++ public/test-parcels/-103.102.nft/game.ts
@@ -1,8 +1,7 @@
 import { NFTShape, Entity, engine, Transform, Vector3, Color3 } from 'decentraland-ecs/src'
 
 const entity = new Entity()
-const shapeComponent = new NFTShape('ethereum://0x06012c8cf97BEaD5deAe237070F9587f8E7A266d/558536')
-shapeComponent.color = Color3.Green()
+const shapeComponent = new NFTShape('ethereum://0x06012c8cf97BEaD5deAe237070F9587f8E7A266d/558536', Color3.Green())
 entity.addComponent(shapeComponent)
 entity.addComponent(
   new Transform({
