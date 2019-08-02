4eb4a6df462f4f1f14f7c5fe38ba62cdb7792fdb
Esteban Ordano <esteban@decentraland.org>
2019-08-02 03:50:28 -0300

    fix; better example

--- webb/fakelib.tsx
+++ webb/fakelib.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { default as Config } from '@dcl/config'
+
 export function show() {
-  ReactDOM.render(<h1>Hello dough</h1>, document.querySelector('body'))
+  ReactDOM.render(
+    <div>
+      <h1>Hello dough</h1>
+      <pre> {JSON.stringify(Config, null, 2)}</pre>
+    </div>,
+    document.querySelector('body')
+  )
 }
--- webb/static/run.js
+++ webb/static/run.js
@@ -1,3 +1,5 @@
+window.global = window
+
 require(['dcl/webb/fakelib'], function(run) {
   run.show()
 })
