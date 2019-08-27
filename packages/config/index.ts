34d76fb7a5401955df3d12f820d2a1e1878910c9
moliva <oliva.miguelh@gmail.com>
2019-08-27 16:39:48 -0300

    chore: refactoring loading/unloading of scenes

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -79,7 +79,7 @@ export namespace visualConfigurations {
 }
 
 // Entry points
-export const PREVIEW: boolean = !!(global as any).preview
+export const PREVIEW: boolean = true
 export const EDITOR: boolean = !!(global as any).isEditor
 
 export const STATIC_WORLD = location.search.indexOf('STATIC_WORLD') !== -1 || !!(global as any).staticWorld
