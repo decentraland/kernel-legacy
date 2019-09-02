c168bd844d8f5a41a8dda59067d99a73ad232b7e
Esteban Ordano <esteban@decentraland.org>
2019-09-02 18:42:20 -0300

    feat: new servers

--- config/Communications.ts
+++ config/Communications.ts
@@ -21,16 +21,26 @@ export const Communications = {
     {
       urls: 'stun:stun4.l.google.com:19302'
     },
+
     {
-      urls: 'turn:184.73.100.50:3478',
+      urls: 'turn:18.212.100.131:3478',
+      credential: 'passworddcl',
+      username: 'usernamedcl'
+    },
+    {
+      urls: 'turn:54.156.44.154:3478',
+      credential: 'passworddcl',
+      username: 'usernamedcl'
+    },
+    {
+      urls: 'turn:3.219.30.175:3478',
       credential: 'passworddcl',
       username: 'usernamedcl'
     }
   ]
 }
 
-export const PUBLIC_KEY_SOURCE =
-  'https://auth.decentraland.org/api/v1/public_key'
+export const PUBLIC_KEY_SOURCE = 'https://auth.decentraland.org/api/v1/public_key'
 export const PINNED_PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
 MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEd/4dLW7Q0Ik4sp4lKwLNofhgZqvA
 piXr3KibX8Hch8SNmzJZgFsGgQYsS7MAv40owSiL2k6a5bsQ4102Kw++Pw==
