b2e20f8b46862b2e20f541a038f0600067795209
Esteban Ordano <esteban@decentraland.org>
2019-09-04 18:19:19 -0300

    dont trigger cache

--- public/ecs-scenes/-200,110-relative-layout-ui/game.ts
+++ public/ecs-scenes/-200,110-relative-layout-ui/game.ts
@@ -1,8 +1,8 @@
 const SEND_ICON = './images/send-icon.png'
 
 const ui = new UICanvas()
-const _Entity = new Entity()
-const uiEntity = _Entity
+const myEntity = new Entity()
+const uiEntity = myEntity
 uiEntity.addComponentOrReplace(ui)
 engine.addEntity(uiEntity)
 
