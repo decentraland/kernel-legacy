b0cfe9b95fe79fc581be460e5e4c0b1c0246bf0a
Esteban Ordano <esteban@decentraland.org>
2019-09-20 15:00:36 +0200

    Fatal errors & teleport

---
+++ packages/shared/loading/ReportFatalError.ts
@@ -0,0 +1,30 @@
+import { ExecutionLifecycleEvent, COMMS_COULD_NOT_BE_ESTABLISHED, NOT_INVITED, NO_WEBGL_COULD_BE_CREATED } from "./types";
+
+let aborted = false
+
+export function bringDownClientAndShowError(event: ExecutionLifecycleEvent) {
+  if (aborted) {
+    return
+  }
+  const body = document.body
+  const container = document.getElementById('gameContainer')
+  container!.setAttribute('style', 'display: none !important')
+  const progressBar = document.getElementById('progress-bar')
+  progressBar!.setAttribute('style', 'display: none !important')
+
+  body.setAttribute('style', 'background-image: none !important;')
+
+  const targetError = event === COMMS_COULD_NOT_BE_ESTABLISHED ? 'comms'
+    : event === NOT_INVITED ? 'notinvited'
+      : event === NO_WEBGL_COULD_BE_CREATED ? 'notsupported'
+        : 'fatal'
+
+  document.getElementById('error-' + targetError)!.setAttribute('style', 'display: block !important')
+  aborted = true
+}
+
+export function ReportFatalError(event: ExecutionLifecycleEvent) {
+  bringDownClientAndShowError(event)
+}
+
+(global as any).ReportFatalError = ReportFatalError
