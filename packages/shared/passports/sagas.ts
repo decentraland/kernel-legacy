5069f8460ccb088458fb662bbad602732ebcdef8
Esteban Ordano <esteban@decentraland.org>
2019-09-27 01:23:16 +0200

    fix profile pic

--- packages/shared/passports/sagas.ts
+++ packages/shared/passports/sagas.ts
@@ -271,8 +271,8 @@ export async function modifyAvatar(params: {
 
 async function saveSnapshots(userURL: string, accessToken: string, face: string, body: string) {
   const data = new FormData()
-  data.append('face', stringToBlob(face), 'face.png')
-  data.append('body', stringToBlob(body), 'body.png')
+  data.append('face', base64ToBlob(face), 'face.png')
+  data.append('body', base64ToBlob(body), 'body.png')
   return (await fetch(`${userURL}/snapshot`, {
     method: 'POST',
     body: data,
@@ -281,6 +281,26 @@ async function saveSnapshots(userURL: string, accessToken: string, face: string,
     }
   })).json()
 }
-function stringToBlob(str: string) {
-  return new Blob([btoa(str)], { type: 'image/png' })
+
+export function base64ToBlob(base64: string): Blob {
+  const sliceSize = 1024
+  const byteChars = window.atob(base64)
+  const byteArrays = []
+  let len = byteChars.length
+
+  for (let offset = 0; offset < len; offset += sliceSize) {
+    const slice = byteChars.slice(offset, offset + sliceSize)
+
+    const byteNumbers = new Array(slice.length)
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i)
+    }
+
+    const byteArray = new Uint8Array(byteNumbers)
+
+    byteArrays.push(byteArray)
+    len = byteChars.length
+  }
+
+  return new Blob(byteArrays, { type: 'image/jpeg' })
 }
