71220bee939bb6c449dbd6a00a3327ccb8fb6ee6
Esteban Ordano <esteban@decentraland.org>
2019-09-27 19:58:03 +0200

    fix: urlencoding issues

--- packages/shared/passports/transformations/profileToRendererFormat.ts
+++ packages/shared/passports/transformations/profileToRendererFormat.ts
@@ -3,8 +3,18 @@ import { ProfileForRenderer } from 'decentraland-ecs/src'
 import { convertToRGBObject } from './convertToRGBObject'
 import { dropDeprecatedWearables } from './processServerProfile'
 export function profileToRendererFormat(profile: Profile): ProfileForRenderer {
+  const snapshots = profile.snapshots
+    ? {
+        face: profile.snapshots.face.replace('|', '%7C'),
+        body: profile.snapshots.body.replace('|', '%7C')
+      }
+    : {
+        face: '',
+        body: ''
+      }
   return {
     ...profile,
+    snapshots,
     avatar: {
       ...profile.avatar,
       wearables: profile.avatar.wearables.filter(dropDeprecatedWearables),
