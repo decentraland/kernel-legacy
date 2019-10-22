8925406339bd7d6239e4df5b13afbae8c5546618
moliva <oliva.miguelh@gmail.com>
2019-10-22 14:35:16 -0300

    chore: disabling tests to test deployment

--- test/index.ts
+++ test/index.ts
@@ -57,10 +57,11 @@ import './visualValidation/referenceCubes.test'
 
 import * as engine from 'engine'
 import * as renderer from 'engine/renderer'
-declare var mocha: any
+// declare var mocha: any
 declare var global: any
 
 global['engine'] = engine
 global['renderer'] = renderer
 
-mocha.run()
+// XXX - reenable tests before merging - moliva - 22/10/2019
+// mocha.run()
