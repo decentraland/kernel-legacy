c4dbca5afeb95bf62ab4a0bcc8dac35815654e69
moliva <oliva.miguelh@gmail.com>
2019-11-02 01:23:02 -0300

    fix: fixing tests :raised-hands:

--- test/unit/passport.saga.test.tsx
+++ test/unit/passport.saga.test.tsx
@@ -8,11 +8,10 @@ import {
   profileServerRequest
 } from 'shared/passports/sagas'
 import { getProfile, getProfileDownloadServer } from 'shared/passports/selectors'
-import { passportSaga } from '../../packages/shared/passports/sagas'
+import { passportSaga, delay } from '../../packages/shared/passports/sagas'
 import { getCurrentUserId } from '../../packages/shared/auth/selectors'
 import { processServerProfile } from '../../packages/shared/passports/transformations/processServerProfile'
-// import { expect } from 'chai'
-// import { PASSPORT_REQUEST, PASSPORT_SUCCESS } from '../../packages/shared/passports/actions'
+import { dynamic } from 'redux-saga-test-plan/providers'
 
 describe('fetchProfile behavior', () => {
   it('behaves normally', () => {
@@ -29,24 +28,28 @@ describe('fetchProfile behavior', () => {
       .run()
   })
 
-  it('runs once for two requests of same user', () => {
+  it('completes once for more than one request of same user', () => {
     const profile = { data: 'profile' }
     return expectSaga(passportSaga)
+      .put(passportSuccess('user|1', 'passport' as any))
+      .not.put(passportSuccess('user|1', 'passport' as any))
+      .dispatch(passportRequest('user|1'))
       .dispatch(passportRequest('user|1'))
       .dispatch(passportRequest('user|1'))
       .provide([
         [select(getProfileDownloadServer), 'server'],
         [select(getAccessToken), 'access-token'],
-        [call(profileServerRequest, 'server', 'user|1', 'access-token'), profile],
+        [
+          call(profileServerRequest, 'server', 'user|1', 'access-token'),
+          dynamic(async () => {
+            await delay(1) // yield control, so that the next event is received and takeLatest cancels the previous
+            return profile
+          })
+        ],
         [select(getCurrentUserId), 'myid'],
         [call(processServerProfile, 'user|1', profile), 'passport']
       ])
       .run()
-      .then(result => {
-        // const { effects } = result
-        // console.log(JSON.stringify(effects.put.filter($ => $.payload.action.type === PASSPORT_SUCCESS)))
-        // expect(effects.put.filter($ => $.payload.action.type === PASSPORT_SUCCESS)).to.be.length(1)
-      })
   })
 
   it('runs one request for each user', () => {
@@ -55,14 +58,28 @@ describe('fetchProfile behavior', () => {
       .put(passportSuccess('user|1', 'passport1' as any))
       .put(passportSuccess('user|2', 'passport2' as any))
       .dispatch(passportRequest('user|1'))
+      .dispatch(passportRequest('user|1'))
+      .dispatch(passportRequest('user|2'))
       .dispatch(passportRequest('user|2'))
       .provide([
         [select(getProfileDownloadServer), 'server'],
         [select(getAccessToken), 'access-token'],
-        [call(profileServerRequest, 'server', 'user|1', 'access-token'), profile],
+        [
+          call(profileServerRequest, 'server', 'user|1', 'access-token'),
+          dynamic(async () => {
+            await delay(1) // yield control, so that the next event is received and takeLatest cancels the previous
+            return profile
+          })
+        ],
         [select(getCurrentUserId), 'myid'],
         [call(processServerProfile, 'user|1', profile), 'passport1'],
-        [call(profileServerRequest, 'server', 'user|2', 'access-token'), profile],
+        [
+          call(profileServerRequest, 'server', 'user|2', 'access-token'),
+          dynamic(async () => {
+            await delay(1) // yield control, so that the next event is received and takeLatest cancels the previous
+            return profile
+          })
+        ],
         [call(processServerProfile, 'user|2', profile), 'passport2']
       ])
       .run()
